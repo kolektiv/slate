54b261050e3b5d1c0ff4154247cb0bb14878878d
kolektiv <andrew@xyncro.com>
2016-07-24 19:46:16 +0100

    updating directory structure, etc.

--- Gemfile
+++ Gemfile
@@ -2,16 +2,17 @@ source 'https://rubygems.org'
 
 # Markdown
 
-gem 'redcarpet', '~> 3.3.2'
+gem 'redcarpet',              '~> 3'
 
 # Middleman
 
-gem 'middleman', '~> 4.0.0'
-gem 'middleman-gh-pages', '~> 0.0.3'
-gem 'middleman-syntax', '~> 2.1.0'
-gem 'middleman-autoprefixer', '~> 2.7.0'
-gem "middleman-sprockets", "~> 4.0.0"
+gem 'middleman',              '~> 4'
+gem 'middleman-autoprefixer', '~> 2'
+gem 'middleman-gh-pages',     '~> 0'
+gem 'middleman-livereload',   '~> 3'
+gem 'middleman-sprockets',    '~> 4'
+gem 'middleman-syntax',       '~> 3'
 
 # Syntax
 
-gem 'rouge', '~> 1.10.1'
+gem 'rouge',                  '~> 2'
--- config.rb
+++ config.rb
@@ -1,8 +1,20 @@
+
+# Paths
+
+set :source, 'src'
+set :build_dir, 'bin'
+
+set :css_dir, 'css'
+set :js_dir, 'js'
+set :images_dir, 'img'
+set :fonts_dir, 'font'
+
 # Sprockets
 
 activate :sprockets
 
 # Markdown
+
 set :markdown_engine, :redcarpet
 set :markdown,
     fenced_code_blocks: true,
@@ -13,29 +25,26 @@ set :markdown,
     with_toc_data: true,
     no_intra_emphasis: true
 
-# Assets
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-set :fonts_dir, 'fonts'
+# Syntax
 
-# Activate the syntax highlighter
 activate :syntax
 
+# CSS
+
 activate :autoprefixer do |config|
-  config.browsers = ['last 2 version', 'Firefox ESR']
+  config.browsers = [ 'last 2 version', 'Firefox ESR' ]
   config.cascade  = false
   config.inline   = true
 end
 
-# Github pages require relative links
+# Links
+
 activate :relative_assets
 set :relative_links, true
 
-# Build Configuration
+# Build
+
 configure :build do
-  # If you're having trouble with Middleman hanging, commenting
-  # out the following two lines has been known to help
   activate :minify_css
   activate :minify_javascript
   # activate :relative_assets
@@ -43,6 +52,7 @@ configure :build do
   # activate :gzip
 end
 
-# Deploy Configuration
-# If you want Middleman to listen on a different port, you can set that below
+# Server
+
 set :port, 4567
+activate :livereload
